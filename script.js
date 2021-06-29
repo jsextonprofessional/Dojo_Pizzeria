ef442083ede86ad30b09fe5a43d11798dddec9b3
jsprofessional <jshuman@protonmail.com>
2021-06-28 20:21:39 -0500

    it works now

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var pizza = {
-    crustType: ["deep dish", "hand tossed"],
-    sauceType: ["traditional", "marinara"],
-    cheese: ["mozzarella", "feta"],
-    toppings: ["pepperoni", "sausage", "mushrooms", "olives", "onions"]
-};
+// var pizza = {
+//     crustType: ["deep-dish", "hand-tossed"],
+//     sauceType: ["traditional", "marinara"],
+//     cheese: ["mozzarella", "feta"],
+//     toppings: ["pepperoni", "sausage", "mushrooms", "olives", "onions"]
+// };
 
-console.log(pizza);
+// console.log(pizza);
 
 function pizzaOven(crustType, sauceType, cheese, toppings) {
     var pizza = {};
@@ -15,3 +15,15 @@ function pizzaOven(crustType, sauceType, cheese, toppings) {
     pizza.toppings = toppings;
     return pizza;
 }
+
+var p1 = pizzaOven("deep-dish", "traditional", ["mozzarella"], ["pepperoni", "sausage"]);
+console.log(p1);
+
+var p2 = pizzaOven("hand-tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]);
+console.log(p2);
+
+var p3 = pizzaOven("hand-tossed", "ranch", ["mozzarella"], ["chicken", "bacon"]);
+console.log(p3);
+
+var p4 = pizzaOven("hand-tossed", ["tomato", "olive-oil"], ["fresh-mozz"], ["basil"]);
+console.log(p4)
